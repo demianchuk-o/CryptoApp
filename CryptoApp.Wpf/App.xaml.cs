6ad864f05b7204fdb1731a779779c948ea93aada
demianchuk-o <demyanchukua@gmail.com>
2024-10-29 15:33:23 +0200

    Changed project's name and references for a desktop presentation layer

--- CryptoApp.Wpf/App.xaml.cs
+++ CryptoApp.Wpf/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace CryptoApp;
+namespace CryptoApp.Wpf;
 
 /// <summary>
 /// Interaction logic for App.xaml
--- CryptoApp.Wpf/MainWindow.xaml.cs
+++ CryptoApp.Wpf/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CryptoApp;
+namespace CryptoApp.Wpf;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
