18f4f07084c7ed1ab6753f97644e9c7ab5b71a19
demianchuk-o <demyanchukua@gmail.com>
2024-10-30 19:45:47 +0200

    TopCurrenciesPage basic setup to display the currencies data

--- CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
+++ CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
@@ -1,11 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CryptoApp.Wpf.TopCurrencies;
 
 public partial class TopCurrenciesPage : Page
 {
+    private TopCurrenciesViewModel _viewModel;
     public TopCurrenciesPage()
     {
         InitializeComponent();
+        _viewModel = App.Current.ServiceProvider.GetRequiredService<TopCurrenciesViewModel>();
+        DataContext = _viewModel;
+    }
+
+    protected override async void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+        await _viewModel.LoadDataAsync();
     }
 }
