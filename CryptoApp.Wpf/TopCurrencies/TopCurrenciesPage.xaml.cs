25becea3b1e915d2c89da63b5e0afba6a783cb78
demianchuk-o <demyanchukua@gmail.com>
2024-10-30 21:26:28 +0200

    Make top currencies data happen on Loaded event

--- CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
+++ CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
@@ -6,17 +6,17 @@ namespace CryptoApp.Wpf.TopCurrencies;
 
 public partial class TopCurrenciesPage : Page
 {
-    private TopCurrenciesViewModel _viewModel;
+    private readonly TopCurrenciesViewModel _viewModel;
+
     public TopCurrenciesPage()
     {
         InitializeComponent();
         _viewModel = App.Current.ServiceProvider.GetRequiredService<TopCurrenciesViewModel>();
         DataContext = _viewModel;
     }
-
-    protected override async void OnInitialized(EventArgs e)
+    
+    private async void Page_Loaded(object sender, RoutedEventArgs e)
     {
-        base.OnInitialized(e);
         await _viewModel.LoadDataAsync();
     }
 }
