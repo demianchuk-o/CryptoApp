34ee8ad77bd2e1db9ad7edaa90acb8be1c4fe721
demianchuk-o <demyanchukua@gmail.com>
2024-10-30 16:10:32 +0200

    Top currencies ViewModel setup to load data from a service and update currencies list

---
+++ CryptoApp.Wpf/TopCurrencies/TopCurrenciesViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CryptoApp.Application.Crypto;
+using CryptoApp.Core.CryptoCurrencies;
+
+namespace CryptoApp.Wpf.TopCurrencies;
+
+public class TopCurrenciesViewModel : INotifyPropertyChanged
+{
+    private readonly ICryptoService _cryptoService;
+    public ObservableCollection<CryptoCurrency> CryptoCurrencies { get; } = [];
+    private int _limit = 10;
+    
+    public int Limit
+    {
+        get => _limit;
+        set
+        {
+            if (_limit != value)
+            {
+                _limit = value;
+                OnPropertyChanged(nameof(Limit));
+            }
+        }
+    }
+    
+    public TopCurrenciesViewModel(ICryptoService cryptoService)
+    {
+        _cryptoService = cryptoService;
+    }
+    
+    public async Task LoadDataAsync()
+    {
+        var result = await _cryptoService.GetTopCryptoCurrenciesAsync(Limit);
+        if (result.IsSuccess)
+        {
+            CryptoCurrencies.Clear();
+            foreach (var cryptoCurrency in result.Data)
+            {
+                CryptoCurrencies.Add(cryptoCurrency);
+            }
+        }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
