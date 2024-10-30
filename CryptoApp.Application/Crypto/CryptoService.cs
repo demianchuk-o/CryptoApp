29a9f8368548c3744e1231a165ab9eabdb5c8a36
demianchuk-o <demyanchukua@gmail.com>
2024-10-30 15:57:33 +0200

    Implement CryptoService, which transforms api response data into a domain data with help of custom mapper

---
+++ CryptoApp.Application/Crypto/CryptoService.cs
@@ -0,0 +1,32 @@
+﻿using CryptoApp.Application.Mappers;
+using CryptoApp.Core.CryptoCurrencies;
+using CryptoApp.Core.Results;
+using CryptoApp.Infrastructure.API.CoinCap;
+
+namespace CryptoApp.Application.Crypto;
+
+public class CryptoService : ICryptoService
+{
+    private readonly ICoinCapApiClient _coinCapApiClient;
+
+    public CryptoService(ICoinCapApiClient coinCapApiClient)
+    {
+        _coinCapApiClient = coinCapApiClient;
+    }
+
+    public async Task<Result<List<CryptoCurrency>>> GetTopCryptoCurrenciesAsync(int limit = 10)
+    {
+        var result = await _coinCapApiClient.GetAssetsAsync(limit);
+        if (result.IsSuccess)
+        {
+            var responseData = result.Data;
+            var cryptoCurrencies = responseData.Data
+                .Select(CryptoCurrencyMapper.Map)
+                .ToList();
+            
+            return Result<List<CryptoCurrency>>.Success(cryptoCurrencies);
+        }
+        
+        return Result<List<CryptoCurrency>>.Failure(result.Message);
+    }
+}
