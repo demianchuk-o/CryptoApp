c58a776d56e867116b100f8b83183d26b24d2c84
demianchuk-o <demyanchukua@gmail.com>
2024-10-31 20:02:53 +0200

    Value converters to format the data about currency

---
+++ CryptoApp.Wpf/Shared/ValueConverters/PriceUsdConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CryptoApp.Wpf.Shared.ValueConverters;
+
+public class PriceUsdConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is decimal priceUsd)
+        {
+            return priceUsd.ToString("C2", CultureInfo.InvariantCulture);
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string priceString && decimal.TryParse(priceString, NumberStyles.Currency, CultureInfo.InvariantCulture, out var priceUsd))
+        {
+            return priceUsd;
+        }
+        return value;
+    }
+}
---
+++ CryptoApp.Wpf/Shared/ValueConverters/VolumeUsd24HrConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CryptoApp.Wpf.Shared.ValueConverters;
+
+public class VolumeUsd24HrConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is decimal volumeUsd)
+        {
+            return volumeUsd.ToString("N0", CultureInfo.InvariantCulture);
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string volumeString && decimal.TryParse(volumeString, NumberStyles.Any, CultureInfo.InvariantCulture, out var volumeUsd))
+        {
+            return volumeUsd;
+        }
+        return value;
+    }
+}
