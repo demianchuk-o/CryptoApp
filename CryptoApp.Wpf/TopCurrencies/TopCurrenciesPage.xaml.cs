4ae63bead8cd322a0cd5a0be1a8327064de5c76e
demianchuk-o <demyanchukua@gmail.com>
2024-11-01 21:06:19 +0200

    Removed unused event handler

--- CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
+++ CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
@@ -14,9 +14,5 @@ public partial class TopCurrenciesPage : Page
         _viewModel = App.Current.ServiceProvider.GetRequiredService<TopCurrenciesViewModel>();
         DataContext = _viewModel;
     }
-    
-    private async void Page_Loaded(object sender, RoutedEventArgs e)
-    {
-        await _viewModel.LoadDataAsync();
-    }
+
 }
