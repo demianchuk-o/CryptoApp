be57c1368d99aa2bdd14c066b6e9df0efecf6375
demianchuk-o <demyanchukua@gmail.com>
2024-11-01 22:56:07 +0200

    very quick view and viewmodel setup

---
+++ CryptoApp.Wpf/SearchCurrencies/SearchCurrenciesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoApp.Wpf.SearchCurrencies;
+
+public partial class SearchCurrenciesPage : Page
+{
+    private SearchCurrenciesViewModel _viewModel;
+    public SearchCurrenciesPage()
+    {
+        InitializeComponent();
+        _viewModel = App.Current.ServiceProvider
+            .GetRequiredService<SearchCurrenciesViewModel>();
+        DataContext = _viewModel;
+    }
+}
