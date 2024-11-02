49a2af996f2135d5752918cead7cf9803363b4ac
demianchuk-o <demyanchukua@gmail.com>
2024-11-02 23:08:12 +0200

    hardcoded navigation on top currencies page for proof of concept

--- CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
+++ CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using CryptoApp.Wpf.Shared.Navigation;
+using CryptoApp.Wpf.Shared.Navigation.Manager;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CryptoApp.Wpf.TopCurrencies;
@@ -7,12 +10,19 @@ namespace CryptoApp.Wpf.TopCurrencies;
 public partial class TopCurrenciesPage : Page
 {
     private readonly TopCurrenciesViewModel _viewModel;
-
+    private readonly INavigationService _navigationService;
     public TopCurrenciesPage()
     {
         InitializeComponent();
         _viewModel = App.Current.ServiceProvider.GetRequiredService<TopCurrenciesViewModel>();
+        _navigationService = App.Current.ServiceProvider
+            .GetRequiredService<IFrameNavigationManager>()
+            .GetNavigationService(FrameType.Main);
         DataContext = _viewModel;
     }
 
+    private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        _navigationService.NavigateToDetails("bitcoin");
+    }
 }
