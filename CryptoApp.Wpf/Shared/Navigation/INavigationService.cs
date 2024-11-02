a309533ee233ca4cefb5a50ac1b6a8c784bb2805
demianchuk-o <demyanchukua@gmail.com>
2024-11-02 21:28:11 +0200

    Defined NavigationService, which will provide Frame's navigation to viewmodels

---
+++ CryptoApp.Wpf/Shared/Navigation/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoApp.Wpf.Shared.Navigation;
+
+public interface INavigationService
+{
+    void Initialize(Frame frame);
+    void NavigateTo(string pageUri, object parameter = null);
+    void GoBack();
+    bool CanGoBack { get; }
+}
---
+++ CryptoApp.Wpf/Shared/Navigation/NavigationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoApp.Wpf.Shared.Navigation;
+
+public class NavigationService : INavigationService
+{
+    private Frame _frame;
+    public void Initialize(Frame frame)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NavigateTo(string pageUri, object parameter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GoBack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CanGoBack { get; }
+}
