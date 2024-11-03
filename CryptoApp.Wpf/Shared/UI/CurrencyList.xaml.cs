bf7f8575a1323b8f687e9c1edb48b76b9df7fd19
demianchuk-o <demyanchukua@gmail.com>
2024-11-03 20:00:21 +0200

    defining a user control to reuse a list of search results

---
+++ CryptoApp.Wpf/Shared/UI/CurrencyList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoApp.Wpf.Shared.UI;
+
+public partial class CurrencyList : UserControl
+{
+    public CurrencyList()
+    {
+        InitializeComponent();
+    }
+}
