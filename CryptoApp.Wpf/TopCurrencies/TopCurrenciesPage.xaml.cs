7e7446b0b3e7e76fd79c21d3faef716f2785af2c
demianchuk-o <demyanchukua@gmail.com>
2024-11-03 20:35:51 +0200

    remove debug code

--- CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
+++ CryptoApp.Wpf/TopCurrencies/TopCurrenciesPage.xaml.cs
@@ -10,19 +10,10 @@ namespace CryptoApp.Wpf.TopCurrencies;
 public partial class TopCurrenciesPage : Page
 {
     private readonly TopCurrenciesViewModel _viewModel;
-    private readonly INavigationService _navigationService;
     public TopCurrenciesPage()
     {
         InitializeComponent();
         _viewModel = App.Current.ServiceProvider.GetRequiredService<TopCurrenciesViewModel>();
-        _navigationService = App.Current.ServiceProvider
-            .GetRequiredService<IFrameNavigationManager>()
-            .GetNavigationService(FrameType.Main);
         DataContext = _viewModel;
-    }
-
-    private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-        _navigationService.NavigateToDetails("bitcoin");
     }
 }
