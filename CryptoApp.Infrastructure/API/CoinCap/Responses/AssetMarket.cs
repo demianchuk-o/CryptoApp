6867455236c2ad9320f4a28a9ae0b05c232867dd
demianchuk-o <demyanchukua@gmail.com>
2024-11-04 17:08:06 +0200

    Define response types to get markets and prices to buy

---
+++ CryptoApp.Infrastructure/API/CoinCap/Responses/AssetMarket.cs
@@ -0,0 +1,6 @@
+﻿namespace CryptoApp.Infrastructure.API.CoinCap.Responses;
+
+public record AssetMarket(
+    string exchangeId,
+    string baseId,
+    decimal priceUsd);
---
+++ CryptoApp.Infrastructure/API/CoinCap/Responses/CoinCapGetMarketsResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace CryptoApp.Infrastructure.API.CoinCap.Responses;
+
+public record CoinCapGetMarketsResponse(
+    AssetMarket[] data);
