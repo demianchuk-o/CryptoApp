65cbd994d0c682145db1e5efaa1979e7d31f45b7
demianchuk-o <demyanchukua@gmail.com>
2024-11-04 17:14:21 +0200

    Domain models for market-prices

---
+++ CryptoApp.Core/CryptoCurrencies/MarketPrice.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoApp.Core.CryptoCurrencies;
+
+public class MarketPrice
+{
+    public string ExchangeId { get; set; }
+    public string CurrencyId { get; set; }
+    public decimal PriceUsd { get; set; }
+}
